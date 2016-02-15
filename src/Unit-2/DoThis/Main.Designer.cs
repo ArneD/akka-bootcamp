e05bd3baa46692489d3d7dbd7a6e73aa9a19dc34
Arne Dumarey <dumarey.arne@gmail.com>
2016-02-15 19:44:19 +0100

    Finished 2.3

--- src/Unit-2/DoThis/Main.Designer.cs
+++ src/Unit-2/DoThis/Main.Designer.cs
@@ -28,47 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.sysChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.AddSeriesButton = new System.Windows.Forms.Button();
+            this.MonitorCpuButton = new System.Windows.Forms.Button();
+            this.MonitorMemoryButton = new System.Windows.Forms.Button();
+            this.MonitorDiskButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.sysChart)).BeginInit();
             this.SuspendLayout();
             // 
             // sysChart
             // 
-            chartArea1.Name = "ChartArea1";
-            this.sysChart.ChartAreas.Add(chartArea1);
+            chartArea4.Name = "ChartArea1";
+            this.sysChart.ChartAreas.Add(chartArea4);
             this.sysChart.Dock = System.Windows.Forms.DockStyle.Fill;
-            legend1.Name = "Legend1";
-            this.sysChart.Legends.Add(legend1);
+            legend4.Name = "Legend1";
+            this.sysChart.Legends.Add(legend4);
             this.sysChart.Location = new System.Drawing.Point(0, 0);
             this.sysChart.Name = "sysChart";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.sysChart.Series.Add(series1);
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.sysChart.Series.Add(series4);
             this.sysChart.Size = new System.Drawing.Size(684, 446);
             this.sysChart.TabIndex = 0;
             this.sysChart.Text = "sysChart";
             // 
-            // AddSeriesButton
+            // MonitorCpuButton
             // 
-            this.AddSeriesButton.Location = new System.Drawing.Point(583, 388);
-            this.AddSeriesButton.Name = "AddSeriesButton";
-            this.AddSeriesButton.Size = new System.Drawing.Size(75, 23);
-            this.AddSeriesButton.TabIndex = 1;
-            this.AddSeriesButton.Text = "Add";
-            this.AddSeriesButton.UseVisualStyleBackColor = true;
-            this.AddSeriesButton.Click += new System.EventHandler(this.AddSeriesButton_Click);
+            this.MonitorCpuButton.Location = new System.Drawing.Point(573, 261);
+            this.MonitorCpuButton.Name = "MonitorCpuButton";
+            this.MonitorCpuButton.Size = new System.Drawing.Size(99, 45);
+            this.MonitorCpuButton.TabIndex = 1;
+            this.MonitorCpuButton.Text = "CPU (ON)";
+            this.MonitorCpuButton.UseVisualStyleBackColor = true;
+            this.MonitorCpuButton.Click += new System.EventHandler(this.MonitorCpuButton_Click);
+            // 
+            // MonitorMemoryButton
+            // 
+            this.MonitorMemoryButton.Location = new System.Drawing.Point(573, 312);
+            this.MonitorMemoryButton.Name = "MonitorMemoryButton";
+            this.MonitorMemoryButton.Size = new System.Drawing.Size(99, 45);
+            this.MonitorMemoryButton.TabIndex = 2;
+            this.MonitorMemoryButton.Text = "MEMORY (OFF)";
+            this.MonitorMemoryButton.UseVisualStyleBackColor = true;
+            this.MonitorMemoryButton.Click += new System.EventHandler(this.MonitorMemoryButton_Click);
+            // 
+            // MonitorDiskButton
+            // 
+            this.MonitorDiskButton.Location = new System.Drawing.Point(573, 363);
+            this.MonitorDiskButton.Name = "MonitorDiskButton";
+            this.MonitorDiskButton.Size = new System.Drawing.Size(99, 45);
+            this.MonitorDiskButton.TabIndex = 3;
+            this.MonitorDiskButton.Text = "DISK (OFF)";
+            this.MonitorDiskButton.UseVisualStyleBackColor = true;
+            this.MonitorDiskButton.Click += new System.EventHandler(this.MonitorDiskButton_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(684, 446);
-            this.Controls.Add(this.AddSeriesButton);
+            this.Controls.Add(this.MonitorDiskButton);
+            this.Controls.Add(this.MonitorMemoryButton);
+            this.Controls.Add(this.MonitorCpuButton);
             this.Controls.Add(this.sysChart);
             this.Name = "Main";
             this.Text = "System Metrics";
@@ -82,7 +106,9 @@
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart sysChart;
-        private System.Windows.Forms.Button AddSeriesButton;
+        private System.Windows.Forms.Button MonitorCpuButton;
+        private System.Windows.Forms.Button MonitorMemoryButton;
+        private System.Windows.Forms.Button MonitorDiskButton;
     }
 }
 
--- src/Unit-2/DoThis/Main.cs
+++ src/Unit-2/DoThis/Main.cs
@@ -11,7 +11,9 @@ namespace ChartApp
     public partial class Main : Form
     {
         private IActorRef _chartActor;
-        private readonly AtomicCounter _seriesCounter = new AtomicCounter(1);
+        private IActorRef _coordinatorActor;
+        private Dictionary<CounterType, IActorRef> _toggleActors = new Dictionary<CounterType,
+            IActorRef>();
 
         public Main()
         {
@@ -23,12 +25,31 @@ namespace ChartApp
 
         private void Main_Load(object sender, EventArgs e)
         {
-            _chartActor = Program.ChartActors.ActorOf(Props.Create(() => new ChartingActor(sysChart)), "charting");
-            var series = ChartDataHelper.RandomSeries("FakeSeries" + _seriesCounter.GetAndIncrement());
-            _chartActor.Tell(new ChartingActor.InitializeChart(new Dictionary<string, Series>()
-            {
-                {series.Name, series}
-            }));
+            _chartActor = Program.ChartActors.ActorOf(Props.Create(() =>
+        new ChartingActor(sysChart)), "charting");
+            _chartActor.Tell(new ChartingActor.InitializeChart(null)); //no initial series
+
+            _coordinatorActor = Program.ChartActors.ActorOf(Props.Create(() =>
+                    new PerformanceCounterCoordinatorActor(_chartActor)), "counters");
+
+            // CPU button toggle actor
+            _toggleActors[CounterType.Cpu] = Program.ChartActors.ActorOf(
+                Props.Create(() => new ButtonToggleActor(_coordinatorActor, MonitorCpuButton, CounterType.Cpu,
+                    false)).WithDispatcher("akka.actor.synchronized-dispatcher"));
+
+            // MEMORY button toggle actor
+            _toggleActors[CounterType.Memory] = Program.ChartActors.ActorOf(
+               Props.Create(() => new ButtonToggleActor(_coordinatorActor, MonitorMemoryButton,
+                CounterType.Memory, false))
+                .WithDispatcher("akka.actor.synchronized-dispatcher"));
+
+            // DISK button toggle actor
+            _toggleActors[CounterType.Disk] = Program.ChartActors.ActorOf(
+               Props.Create(() => new ButtonToggleActor(_coordinatorActor, MonitorDiskButton, CounterType.Disk,
+                false)).WithDispatcher("akka.actor.synchronized-dispatcher"));
+
+            // Set the CPU toggle to ON so we start getting some data
+            _toggleActors[CounterType.Cpu].Tell(new ButtonToggleActor.Toggle());
         }
 
         private void Main_FormClosing(object sender, FormClosingEventArgs e)
@@ -42,10 +63,19 @@ namespace ChartApp
 
         #endregion
 
-        private void AddSeriesButton_Click(object sender, EventArgs e)
+        private void MonitorCpuButton_Click(object sender, EventArgs e)
         {
-            var series = ChartDataHelper.RandomSeries("FakeSeries" + _seriesCounter.GetAndIncrement());
-            _chartActor.Tell(new ChartingActor.AddSeries(series));
+            _toggleActors[CounterType.Cpu].Tell(new ButtonToggleActor.Toggle());
+        }
+
+        private void MonitorMemoryButton_Click(object sender, EventArgs e)
+        {
+            _toggleActors[CounterType.Memory].Tell(new ButtonToggleActor.Toggle());
+        }
+
+        private void MonitorDiskButton_Click(object sender, EventArgs e)
+        {
+            _toggleActors[CounterType.Disk].Tell(new ButtonToggleActor.Toggle());
         }
     }
 }
